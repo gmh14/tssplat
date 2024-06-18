365178cccb7b78a6cf668c4606cd323bd69a4463
Minghao Guo <guomh2014@gmail.com>
2024-06-18 13:46:55 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@ python -m pip install scikit-build
 pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --index-url https://download.pytorch.org/whl/cu118
 
 # install pypgo
+if [ -z "${CUDA_HOME}" ]; then
+  echo "CUDA_HOME is not defined!"
+  exit 1
+else
+  echo "CUDA_HOME is defined as ${CUDA_HOME}"
+fi
 export PATH=${CUDA_HOME}/bin:${PATH}
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
 
