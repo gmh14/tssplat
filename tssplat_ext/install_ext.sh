b3f5f703058ad5a5e1f0830078f8ff52d939b5e1
Minghao Guo <guomh2014@gmail.com>
2024-06-18 13:47:04 -0700

    Update install_ext.sh

--- tssplat_ext/install_ext.sh
+++ tssplat_ext/install_ext.sh
@@ -1,3 +1,9 @@
+if [ -z "${CUDA_HOME}" ]; then
+  echo "CUDA_HOME is not defined!"
+  exit 1
+else
+  echo "CUDA_HOME is defined as ${CUDA_HOME}"
+fi
 export PATH=${CUDA_HOME}/bin:${PATH}
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
 pip install -v -e .
